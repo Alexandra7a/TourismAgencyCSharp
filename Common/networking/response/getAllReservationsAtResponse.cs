f1124829c6f751c2a92c55ad9468925930853141
Alexandra7a <atoth1433@gmail.com>
2024-05-10 11:29:19 +0300

    functional observer

--- Common/networking/response/getAllReservationsAtResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Common.networking.response
-{
-    [Serializable]
-    public class getAllReservationsAtResponse :IResponse
-    {
-        public int no;
-        public getAllReservationsAtResponse(int no) { this.no = no; }
-    }
-}
